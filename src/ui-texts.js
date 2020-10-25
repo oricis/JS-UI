9eae13de3bf599cf48da9f9fecc2d6f765524b80
moises.alcocer <contacto@ironwoods.es>
2020-10-25 23:59:39 +0100

    refactor: new JS file extracted from ui.js

---
+++ src/ui-texts.js
@@ -0,0 +1,40 @@
+/**
+ * JS-UI
+ *
+ * Moisés Alcocer, 2020
+ * https://www.ironwoods.es
+ * MIT Licence
+ */
+
+
+const addTextById = (id, content) => {
+    let actual_content = $(id).innerText;
+    $('#' + id).innerText = actual_content + content;
+}
+
+const removeText = (selector, position = 0) => {
+    let target = $(selector);
+
+    if (target && target.length > 1) {
+        if (position === 'all') {
+            target.forEach(element => {
+                element.innerText = '';
+            });
+
+        } else {
+            target[position].innerText = '';
+        }
+
+        return;
+    }
+
+    target.innerText = '';
+}
+
+const removeTextById = (id) => {
+    $('#' + id).innerText = '';
+}
+
+const setTextById = (id, content) => {
+    $('#' + id).innerText = content;
+}
