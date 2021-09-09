c0994bbafbfa42b6d4dd89ef474dd0e36abfac55
orici <m_alc1@hotmail.com>
2021-09-09 23:15:44 +0200

    updated example JS

--- tests/assets/js/componentButtonsTest.js
+++ tests/assets/js/componentButtonsTest.js
@@ -1,39 +1,70 @@
-console.log('Loaded script to test "ComponentButtons.js"');
+event('Loaded script to test "ComponentButtons.js"', 'loaded');
+
+const MONTHS_SELECTION_COMPONENT_SELECTOR = '.component.months-selector';
+
+const componentButtons = new ComponentButtons(MONTHS_SELECTION_COMPONENT_SELECTOR);
+componentButtons.handleAcceptButtonActivation(clickedAccept);
+componentButtons.handleCancelButtonActivation(clickedCancel);
+handleCheckboxes();
+handleSelect();
+
 
 function clickedAccept() // void
 {
-    const select = qs('select');
     const checkboxes = qsa('input[type="checkbox"]');
-
-    disable(select);
-    checkboxes.forEach(node => {
-        disable(node);
+    const selectedMonths = [];
+    checkboxes.forEach(checkbox => // void
+    {
+        if (checkbox.checked) {
+            selectedMonths.push(checkbox.value);
+        }
     });
+
+    saveData(selectedMonths);
+
+    function saveData(selectedMonths) // void
+    {
+        log('HACK: Selected months: ', selectedMonths)
+    }
 }
 function clickedCancel() // cancel
 {
-    const select = qs('select');
     const checkboxes = qsa('input[type="checkbox"]');
-
-    enable(select);
-    checkboxes.forEach(node => {
-        enable(node);
+    checkboxes.forEach(checkbox => // void
+    {
+        checkbox.checked = false;
     });
 }
+function handleCheckboxes() // void
+{
+    const acceptButton = componentButtons.getAcceptButtonNode();
+    const cancelButton = componentButtons.getCancelButtonNode();
 
-const componentButtons = new ComponentButtons('.component.months-selector');
-const acceptButton = componentButtons.getAcceptButtonNode();
-const cancelButton = componentButtons.getCancelButtonNode();
-console.log(acceptButton, cancelButton)
-if (acceptButton) {
-    acceptButton.onclick = () => {
-        console.log('CLICKED "accept" button');
-        componentButtons.handleAcceptButtonActivation(clickedAccept);
+    const monthlyCheckboxes
+        = qsa(MONTHS_SELECTION_COMPONENT_SELECTOR + ' [type="checkbox"]');
+    monthlyCheckboxes.forEach(checkbox => // void
+    {
+        checkbox.onclick = () => // void
+        {
+            handleCheckboxActivation(checkbox);
+        }
+    });
+
+    function handleCheckboxActivation(checkbox) // void
+    {
+        if (checkbox.checked) {
+            enable(acceptButton);
+            enable(cancelButton);
+        }
     }
 }
-if (cancelButton) {
-    cancelButton.onclick = () => {
-        console.log('CLICKED "cancel" button');
-        componentButtons.handleCancelButtonActivation(clickedCancel);
+function handleSelect() // void
+{
+    const select = qs('select');
+    if (select) {
+        select.onchange = () => // void
+        {
+            event('handleSelect has changed', 'onchange');
+        }
     }
 }
