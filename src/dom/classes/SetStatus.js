dfcbe94f36027f06872aeef1bdb8cd4c80d6b7fd
Moises Alcocer <m_alc1@hotmail.com>
2021-10-31 12:44:53 +0100

    fixed

--- src/dom/classes/SetStatus.js
+++ src/dom/classes/SetStatus.js
@@ -3,14 +3,14 @@ class SetStatus // class
 
     activate(nodeOrSelector, className = 'active') // void
     {
-        node = getNode(nodeOrSelector);
+        const node = getNode(nodeOrSelector);
         if (node) {
             node.classList.add(className);
         }
     }
     deactivate(nodeOrSelector, className = 'active') // void
     {
-        node = getNode(nodeOrSelector);
+        const node = getNode(nodeOrSelector);
         if (node) {
             node.classList.remove(className);
         }
@@ -18,14 +18,14 @@ class SetStatus // class
 
     disable(nodeOrSelector) // void
     {
-        node = getNode(nodeOrSelector);
+        const node = getNode(nodeOrSelector);
         if (node) {
             node.disabled = true;
         }
     }
     enable(nodeOrSelector) // void
     {
-        node = getNode(nodeOrSelector);
+        const node = getNode(nodeOrSelector);
         if (node) {
             node.removeAttribute('disabled');
         }
@@ -33,14 +33,14 @@ class SetStatus // class
 
     hide(nodeOrSelector) // void
     {
-        node = getNode(nodeOrSelector);
+        const node = getNode(nodeOrSelector);
         if (node) {
             node.hidden = true;
         }
     }
     show(nodeOrSelector) // void
     {
-        node = getNode(nodeOrSelector);
+        const node = getNode(nodeOrSelector);
         if (node) {
             node.removeAttribute('hidden');
         }
